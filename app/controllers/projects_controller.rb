559e1dd33ca659e27838970b598263626dea2463
timfesta <timothyfesta@yahoo.com>
2017-05-12 05:37:32 -0700

    added first controller and views

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,8 @@
+class ProjectsController < ApplicationController
+
+	def demo
+		redirect_to test_path
+	end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
  
  root 'welcome#index'
+
+ get 'office' => 'projects#office'
+ get 'test' => 'projects#test'
+ resources :projects
  
 end
